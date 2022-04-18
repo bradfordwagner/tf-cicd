4a9fed6cb5388b612a5f1e9d4deb2720180793da
Bradford Wagner <bwagner@blackrock.com>
2022-04-18 17:49:19 -0400

    Add Kind Nodeport

--- kind.sh
+++ kind.sh
@@ -3,18 +3,54 @@
 # puts kube contexts into ~/.kube/kind/ or ~/.kube/kind/internal
 # internal signifies in cluster access
 
-function create_cluster() {
-  cluster_name=${1}
+function create_admin_cluster() {
+  cluster_name=$1
+  argocd=30001
   kind create cluster --kubeconfig ~/.kube/kind/${cluster_name} --config /dev/stdin <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 name: ${cluster_name}
 nodes:
 - role: control-plane
+  extraPortMappings:
+  # argocd
+  - containerPort: ${argocd}
+    hostPort: ${argocd}
+    protocol: TCP
 - role: worker
 - role: worker
 - role: worker
 EOF
+}
+
+function create_cicd_cluster() {
+  cluster_name=$1
+  node_port=30000
+  argo_workflows=30002
+  kind create cluster --kubeconfig ~/.kube/kind/${cluster_name} --config /dev/stdin <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+name: ${cluster_name}
+nodes:
+- role: control-plane
+  extraPortMappings:
+  # github webhook
+  - containerPort: ${node_port}
+    hostPort: ${node_port}
+    protocol: TCP
+  # argo workflows
+  - containerPort: ${argo_workflows}
+    hostPort: ${argo_workflows}
+    protocol: TCP
+- role: worker
+- role: worker
+- role: worker
+EOF
+}
+
+function create_cluster() {
+  cluster_name=${1}
+  create_${cluster_name}_cluster ${cluster_name}
   kind get kubeconfig --internal --name ${cluster_name} > ~/.kube/kind/internal/${cluster_name}
 }
 
